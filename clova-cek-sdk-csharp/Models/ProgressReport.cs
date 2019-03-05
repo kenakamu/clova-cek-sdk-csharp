ee3268aa9677dedfb4f69e372a3b657ecbd9dc17
T.Hirabayashi <baa_ram_ewe_shipping@live.jp>
2019-03-05 13:24:53 +0900

    Add Payload models for AudioPlayer

---
+++ clova-cek-sdk-csharp/Models/ProgressReport.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace CEK.CSharp.Models
+{
+    /// <summary>
+    /// 再生後に再生状態情報のレポートを受けるための情報を持ったオブジェクト
+    /// </summary>
+    public class ProgressReport
+    {
+        /// <summary>
+        /// 再生が開始してから、指定された時間が経過した後に、再生状態をレポートするように指定する値です。ミリ秒単位で、このフィールドの値はnullの場合があります。
+        /// </summary>
+        [JsonProperty("progressReportDelayInMilliseconds")]
+        public object ProgressReportDelayInMilliseconds { get; set; }
+        /// <summary>
+        /// 再生中に、指定された間隔ごとに再生状態をレポートするように指定する値です。ミリ秒単位で、このフィールドの値はnullの場合があります。
+        /// </summary>
+        [JsonProperty("progressReportIntervalInMilliseconds")]
+        public object ProgressReportIntervalInMilliseconds { get; set; }
+        /// <summary>
+        /// 再生中に、指定された再生位置を経過する度に、再生状態をレポートするように指定する値です。ミリ秒単位で、このフィールドの値はnullの場合があります。
+        /// </summary>
+        [JsonProperty("progressReportPositionInMilliseconds")]
+        public object ProgressReportPositionInMilliseconds { get; set; }
+    }
+}
---
+++ clova-cek-sdk-csharp/Models/Source.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace CEK.CSharp.Models
+{
+    /// <summary>
+    /// オーディオストリーミングサービスの提供元
+    /// </summary>
+    public class Source
+    {
+        /// <summary>
+        /// オーディオストリーミングサービスのロゴ画像のURL。このフィールドがなかったり、またはフィールド値が空の場合や、ロゴ画像を表示できない場合、source.nameフィールド内のオーディオストリーミングサービスの名前を表示する必要があります。
+        /// </summary>
+        [JsonProperty("logoUrl")]
+        public string LogoUrl { get; set; }
+        /// <summary>
+        /// オーディオストリーミングサービスの名前
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
